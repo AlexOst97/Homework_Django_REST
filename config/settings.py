6d2b3396262301ef52b3a3313a10d64fdffe06e6
unknown <sanya@example.com>
2025-07-09 23:01:26 +0700

    add_35

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ load_dotenv(override=True)
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = "django-insecure-5$pwvghg$tj0&k9llsm2y@h8zg@b(78gsouo)9e$p3+s&$o9pf"
 
 DEBUG = True if os.getenv("DEBUG") == "True" else False
 
